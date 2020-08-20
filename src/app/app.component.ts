2759775e53cfb50c6d0a95383ce8b422720f3734
prabhuinbarajan <inbarajan.prabhu@gmail.com>
2020-08-20 00:03:34 -0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy, ViewChild } from '@angular/core'
 import { FormGroup } from '@angular/forms';
 import { FormlyFormOptions, FormlyFieldConfig } from '@ngx-formly/core/';
+import { SplitComponent, SplitAreaDirective } from 'angular-split'
 
 //"fulfillmentInstruction": "<![CDATA[PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxGWVNfRGF0YUVudmVsb3BlPjxDb25maWd1cmF0aW9uPjxTZW5kZXJJbmZvcm1hdGlvbiBGWVNfQ2xpZW50SUQ9IjY4NTIiLz48L0NvbmZpZ3VyYXRpb24+PERvY3VtZW50cz48RG9jdW1lbnQ+PEdpZnRDYXJkIEZZU19SZXRhaWxlcklEPSI2MDMwMDAwODk2OSIgRllTX1Byb2dyYW1JRD0iMjQ5NzgiLz48L0RvY3VtZW50PjwvRG9jdW1lbnRzPjwvRllTX0RhdGFFbnZlbG9wZT4=]]>",
 
@@ -9,6 +10,22 @@ import { FormlyFormOptions, FormlyFieldConfig } from '@ngx-formly/core/';
   templateUrl: './app.component.html',
 })
 export class AppComponent {
+  @ViewChild('split', {  }) split: SplitComponent
+  @ViewChild('area1', {  }) area1: SplitAreaDirective
+  @ViewChild('area2', {  }) area2: SplitAreaDirective
+
+  direction: string = 'horizontal'
+  sizes = {
+    percent: {
+      area1: 30,
+      area2: 70,
+    },
+    pixel: {
+      area1: 120,
+      area2: '*',
+      area3: 160,
+    },
+  }
   form = new FormGroup({});
   model: any = {
     "productTemplateId": "07675012345",
@@ -65,6 +82,28 @@ export class AppComponent {
   
 
   options: FormlyFormOptions = {};
+  fieldSku: FormlyFieldConfig[] = [
+    {
+      key: 'Textarea',
+      type: 'textarea',
+      templateOptions: {
+        label: 'Private Key',
+        placeholder: 'place your private key here',
+        description: 'private key',
+        rows: 5,
+        required: true,
+      },
+    },
+    {
+      type: 'input',
+      key: 'pemKey',
+      templateOptions: {
+        label: 'Private Key Password',
+        type: 'password' // or 'email'
+      }  
+    }
+  ];
+
 
   fields: FormlyFieldConfig[] = [
     {
@@ -195,9 +234,36 @@ export class AppComponent {
     },
     {
       key: 'fulfillment',
-     //wrappers: ['panel'],
+      wrappers: ['panel'],
       templateOptions: { label: 'fulfillment' },
       fieldGroup: [
+        {
+          key: 'FYS_ClientID',
+          type: 'input',
+          templateOptions: {
+              required: true,
+              type: 'text',
+              label: 'FYS_ClientID',
+          },
+        }, 
+        {
+          key: 'FYS_RetailerID',
+          type: 'input',
+          templateOptions: {
+              required: true,
+              type: 'text',
+              label: 'FYS_RetailerID',
+          },
+        }, 
+        {
+          key: 'FYS_ProgramID',
+          type: 'input',
+          templateOptions: {
+              required: true,
+              type: 'text',
+              label: 'FYS_ProgramID',
+          },
+        }, 
         {
           key: 'fulfillmentPartner',
           type: 'input',
@@ -276,6 +342,17 @@ export class AppComponent {
 
   submit() {
     alert(JSON.stringify(this.model));
+  }
+
+  dragEnd(unit, { sizes }) {
+    if (unit === 'percent') {
+      this.sizes.percent.area1 = sizes[0]
+      this.sizes.percent.area2 = sizes[1]
+    } else if (unit === 'pixel') {
+      this.sizes.pixel.area1 = sizes[0]
+      this.sizes.pixel.area2 = sizes[1]
+      this.sizes.pixel.area3 = sizes[2]
+    }
   }
 }
 
